bedaf193e22c61cafc5a002664cd03d40fd9b1a3
juniuszhou <junius.zhou@gmail.com>
2023-03-28 13:47:50 +0800

    filter log works

--- relayer/main.ts
+++ relayer/main.ts
@@ -1,16 +1,26 @@
 // Setup
-import { Network, Alchemy } from "alchemy-sdk";
+import { Network, Alchemy, Filter, Utils } from "alchemy-sdk";
 import dotenv from "dotenv";
-import {getAccount} from "./src/ethereum";
+import {getWallet, getContract} from "./src/ethereum";
 import {getBasic} from "./src/cosmos";
 
 const main = async () => {
-    try {
         dotenv.config();
     const mnemonic = process.env["MNEMONIC"];
     const apiKey = process.env["API_KEY"];
 
+    if (apiKey == null) {
+      return;
+    }
+    console.log("api key is ", apiKey);
     await getBasic();
+    await getContract(apiKey);
+
+    // return;
+
+    if (mnemonic !== null) {
+      return;
+    }
 
     console.log("api key is ", apiKey);
     const settings = {
@@ -18,32 +28,85 @@ const main = async () => {
         network: Network.ETH_GOERLI,
     };
     const alchemy = new Alchemy(settings);
-    const address = getAccount();
-    const balance = await alchemy.core.getBalance(address);
+
+    console.log('++++++ alchemy url is:', alchemy.config.url);
+
+    const wallet = await getWallet();
+    if (wallet === "") {
+      return
+    }
+
+    const balance = await alchemy.core.getBalance(wallet.getAddress());
     console.log('balance is: ', balance.toString());
 
     // Get the latest block
     const latestBlock = await alchemy.core.getBlockNumber();
     const transactions = (await alchemy.core.getBlock(latestBlock - 10)).transactions;
 
-    console.log("there are %d transactions in block ", transactions.length);
+    // https://emn178.github.io/online-tools/keccak_256.html
+    const method = "Transfer(address,address,uint256)";
+    const topic = "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
-    // for (const tx of transactions) {
-    //     const transaction = await alchemy.core.getTransaction(tx);
-    //     console.log('', transaction?.hash);
-
-    //      const receipt = await alchemy.core.getTransactionReceipt(tx);
-    //     if (receipt != null) {
-    //         console.log("address is ", receipt.contractAddress);
-    //     };
-    // }
     
+    const filter: Filter = {
+      fromBlock: 0,
+      toBlock: 1,
+      address: "",
+      topics: [topic],
+    };
+
+    // Utils.
+
+    let abi = [
+      {
+        constant: true,
+        inputs: [],
+        name: "get",
+        outputs: [
+          {
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+
+    const contractABI = require('../contract-abi.json')
+    const contractAddress = "0x6f3f635A9762B47954229Ea479b4541eAF402A6A";
+
+    // const helloWorldContract = new alchemy.core.getC(
+
+    // };
+
+    // let contractInterface = new Utils.Interface(abi);
+
+    // define the transaction
+    const transaction = {
+      to: "0xa238b6008Bc2FBd9E386A5d4784511980cE504Cd",
+      value: Utils.parseEther("0.001"),
+      gasLimit: "21000",
+      maxPriorityFeePerGas: Utils.parseUnits("5", "gwei"),
+      maxFeePerGas: Utils.parseUnits("20", "gwei"),
+      nonce: await alchemy.core.getTransactionCount(wallet.getAddress()),
+      type: 2,
+      data: "",
+      chainId: 5, // Corresponds to ETH_GOERLI
+    };
+
+    let contract = alchemy.core.call(transaction);
+
+  const rawTransaction = await wallet.signTransaction(transaction);
+  const response = await alchemy.transact.sendTransaction(rawTransaction)
+
+
+    const logs = await alchemy.core.getLogs(filter);
+
+    console.log("there are %d transactions in block ", logs);
+
     
-}   catch {
-
-}
-
-  
 };
 
 main()
--- relayer/src/ethereum.ts
+++ relayer/src/ethereum.ts
@@ -1,8 +1,13 @@
 import { Network, Alchemy, Wallet } from "alchemy-sdk";
 // import ether
+import { Contract, BaseContract, ethers, utils } from "ethers";
+
 import dotenv from "dotenv";
+import { readFileSync } from "fs";
   
-export async function  getAccount() {
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export async function  getWallet() {
     dotenv.config();
     const privKey = process.env["PRIVATE_KEY"];
     if (privKey === undefined) {
@@ -11,7 +16,46 @@ export async function  getAccount() {
     } else {
         let wallet = new Wallet(privKey);
         console.log('address is: ', wallet.address);
-        return wallet.address;
+        if (wallet.address === undefined) {
+            console.log("wallet address is undefined");
+            throw new Error("wallet address is undefined");
+        }
+        return wallet;
     }
     
+}
+
+export async function getContract(alchemyKey: string) {
+    console.log('into getContract ');
+    const content = readFileSync("src/erc20.json");
+
+    const ABI = JSON.parse(content.toString())['abi'];
+
+    const abi = new ethers.utils.Interface(ABI);
+
+    let alchemyUrl = `wss://eth-goerli.g.alchemy.com/v2/` + alchemyKey;
+    const provider = new ethers.providers.WebSocketProvider(alchemyUrl);
+    const from = 8721240;
+    const to = 8721250;
+
+    const filter = [utils.id('Transfer(address,address,uint256)')];
+
+    console.log('filter is: ', filter);
+
+    const logs = await provider.getLogs({
+        fromBlock: 8721249,
+        toBlock: 8721250,
+        address: "0x07865c6e87b9f70255377e024ace6630c1eaa37f",
+        topics: [filter],
+
+    })
+   
+    logs.forEach(log => {   
+        let parsedLog = abi.parseLog(log);
+        console.log('logs is: ', parsedLog);
+    });
+
+
+
+
 }
